c51ae5b11f9b27bfe6dc4773daf65ad898e46284
Aki78 <kmorooka78@gmail.com>
2021-01-04 14:13:53 +0200

    done table. next: try out swipable

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import "./App.css";
 import Entry from "./components/entry";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Table } from "react-bootstrap";
 class App extends Component {
   state = {
-    level: "1 ",
     toDo: "",
     toDoListText: [],
     toDoListNum: [],
@@ -23,6 +23,8 @@ class App extends Component {
     var joinedNum = [...this.state.toDoListNum];
     joinedNum.push(e);
     this.setState({ toDoListNum: joinedNum });
+
+    this.setState({ toDo: "" });
   };
   delList = (delIndex) => {
     let a = [...this.state.toDoListText];
@@ -32,12 +34,50 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Entry setText={this.setText} setLevel={this.setLevel} />
-        {this.state.toDoListText.map((elem, index) => (
-          <Container key={index} sm={8}>
-            <Button onClick={() => this.delList(index)}>{elem}</Button>
-          </Container>
-        ))}
+        <Entry
+          setText={this.setText}
+          setLevel={this.setLevel}
+          toDo={this.state.toDo}
+        />
+
+        <Container className="mt-5">
+          <Table striped size="sm">
+            <tbody className="center">
+              <tr>
+                <th className="myTableWidth align-middle">level</th>
+                <th style={{ fontSize: "25px" }} className="align-middle">
+                  to do
+                </th>
+                <th className="doneTextWidth"></th>
+              </tr>
+              {this.state.toDoListText.map((elem, index) => (
+                <tr key={index}>
+                  <td className="myTableWidth">
+                    <div className="numberCircle">
+                      {this.state.toDoListNum[index]}
+                    </div>
+                  </td>
+                  <td>
+                    <Button
+                      // for="fancy-checkbox-default"
+                      className=" btn btn-default active "
+                    >
+                      <span style={{ fontSize: "20px" }}> {elem} </span>
+                    </Button>
+                  </td>
+                  <td className="doneTextWidth">
+                    <Button
+                      className="btn btn-danger"
+                      onClick={() => this.delList(index)}
+                    >
+                      Done
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
       </div>
     );
   }
--- src/components/entry.jsx
+++ src/components/entry.jsx
@@ -1,10 +1,11 @@
 import Dropdown from "react-bootstrap/Dropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { FaPlusSquare } from "react-icons/fa";
 
 const Entry = (props) => {
   const levels = [1, 2, 3];
   return (
-    <div className="container">
+    <div className="container mt-5">
       <div className="input-group">
         <input
           type="text"
@@ -13,10 +14,11 @@ const Entry = (props) => {
           aria-label="Task"
           aria-describedby="basic-addon1"
           onChange={props.setText}
+          value={props.toDo}
         />
         <Dropdown>
           <Dropdown.Toggle variant="success" id="dropdown-basic">
-            Importance
+            <FaPlusSquare size={20} />
           </Dropdown.Toggle>
 
           <Dropdown.Menu>
